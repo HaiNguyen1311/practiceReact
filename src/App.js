a15fbd87d18265226dee555e6b58fe90ae6e3164
unknown <hainguyen4228@gmail.com>
2023-05-25 16:39:35 +0700

    Add new project practiceReact

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import './App.scss';
+import Header from './components/Header';
+import Container from 'react-bootstrap/Container';
+import { ToastContainer } from 'react-toastify';
+import { useContext } from 'react';
+import { UserContext  } from './context/UserContext';
+import { useEffect } from 'react';
+import AppRoutes from './routes/AppRoutes';
+
+function App() {
+  const { user, loginContext } = useContext(UserContext);
+  useEffect(() => {
+    if(localStorage.getItem('token')) {
+      loginContext(localStorage.getItem('email'), localStorage.getItem('token'))
+    }
+  }, [])
+  return (
+    <>
+      <div className='app-container'>
+        <Header/>
+        <Container>
+          <AppRoutes/>
+        </Container>
+      </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,6 @@
+const Home = () => {
+    return (<>
+        sss
+    </>)
+}
+export default Home
